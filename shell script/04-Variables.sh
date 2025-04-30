cfbce1824867055e81e49c4a66f27fedda1f7795
nagasai305 <nagasai30517@gmail.com>
2025-04-30 10:44:47 +0200

    Fourth sheell

---
+++ shell script/04-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2 , How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine. How are You?"
+echo "$PERSON1: I am fine. What is going"
+echo "$PERSON2: I am also fine. It is going"
