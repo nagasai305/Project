3a3f84a27e2da48d3bca314017b353cf4b7df5e2
nagasai305 <nagasai30517@gmail.com>
2025-04-30 12:00:15 +0200

    datatypes in shell

--- shell script/08-Datatypes.sh
+++ shell script/08-Datatypes.sh
@@ -6,6 +6,8 @@ FRUITES=("apples" "bananas" "grapes" "mangos" "kivi" "orange")
 echo "First Fruit:${FRUITES[0]}"
 echo "Second Fruit:${FRUITES[1]}"
 echo "Third Fruit:${FRUITES[2]}"
+echo "Fourth Fruit:${FRUITES[3]}"
+
 
 
 
