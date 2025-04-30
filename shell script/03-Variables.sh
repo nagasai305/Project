415fe44a8736258ed62e0f6b0e90841366cbc2db
nagasai305 <nagasai30517@gmail.com>
2025-04-30 10:42:27 +0200

    Third sheell

---
+++ shell script/03-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Sai
+PERSON2=Naga
+
+echo "$PERSON1: Hi $PERSON2 , How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine. How are You?"
+echo "$PERSON1: I am fine. What is going"
+echo "$PERSON2: I am also fine. It is going"
